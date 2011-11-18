27d18352063ab21a08d0cf1d8d30f84689d2901e
Carolyn Lea <mrscrl69@fullsail.edu>
2011-11-17 21:59:57 -0700

    at last!

--- dungeonproject/app/_attachments/main.js
+++ dungeonproject/app/_attachments/main.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$.ajax({
 		"url": "_view/dungeons",
 		"dataType": "json",
 		"success": function(data){
 			$.each(data.rows, function(index, dungeon){
+				var dungeon_name = dungeon.value.dungeon_name;
+				var dungeon_type = dungeon.value.dungeon_type;
 				var location = dungeon.value.location;
 				var date_found = dungeon.value.date_found;
+				var populated = dungeon.value.populated;
+				var friendly = dungeon.value.friendly;
 				var occupant = dungeon.value.occupant;
-				var dungeon_name = dungeon.value.dungeon_name;
+				var category = dungeon.value.category;
+				var loot_rating = dungeon.value.loot_rating;
+				var no_loot_here = dungeon.value.no_loot_here;
+				var notes = dungeon.value.notes;
+				
 				$('#dungeonlist').append(
 					$('<li>').append(
 						$('<a>').attr("href", "#")
@@ -19,4 +27,45 @@ $(document).ready(function(){
 			$('#dungeonlist').listview('refresh');
 		}
 	});
+});*/
+
+$('#home').live("pageshow", function(){
+	$.couch.db("dungeonproject").view("dungeonproject/dungeons",{
+		success: function(data) {
+			$('#dungeonlist').empty();
+			$.each(data.rows, function(index, value) {
+				var item = (value.value || value.doc);
+				$('#dungeonlist').append(
+					$('<li>').append(
+						$('<a>')
+							.attr("href", "list.html?list=" + item.dungeon_type)
+							.text(item.dungeon_name)
+						)
+					);
+				});
+				$('#dungeonlist').listview('refresh');
+		}
+	});
 });
+
+/*var urlVars = function () {
+	var urlData = $($.mobile.activePage).data("url");
+	var urlParts = urlData.split('?');
+	var urlPairs = urlParts[1].split('&');
+	var urlValues = {};
+	for (var pair in urlPairs) {
+		var keyValue = urlPairs[pair].split('=');
+		var key = decodeURIComponent(keyValue[0]);
+		var value = decodeURIComponent(keyValue[1]);
+		urlValues[key] = value;
+	}
+	return urlValues;
+};
+
+$('#dlist').live("pageshow", function() {
+	var dungeon = urlVars ()["dungeon"];
+	$.couch.db("dungeonproject").view("dungeonproject/dungeon", {
+		key: "dungeon:" + dungeon
+	});
+});
+*/
--- dungeonproject/app/views/dungeons/map.js
+++ dungeonproject/app/views/dungeons/map.js
@@ -1,10 +1,19 @@
 function(doc){
-	if (doc._id.substr(0,8) === "dungeon:"){
+
 		emit(doc._id.substr(8), {
+			"id": doc._id,
+			"dungeon_name": doc.dungeon_name,
+			"dungeon_type": doc.dungeon_type,
 			"location": doc.location,
 			"date_found": doc.date_found,
+			"populated": doc.populated,
+			"friendly": doc.friendly,
 			"occupant": doc.occupant,
-			"dungeon_name": doc.dungeon_name
+			"category": doc.category,
+			"loot_rating": doc.loot_rating,
+			"no_loot_here": doc.no_loot_here,
+			"notes": doc.notes,
+			"type": doc.type
 		});
-	}
+
 };
